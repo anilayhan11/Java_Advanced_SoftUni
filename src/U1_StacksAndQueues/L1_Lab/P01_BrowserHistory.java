a73ec012a1fcaee70d68a3bd9eb23d8d8ac017dd
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-04-15 21:49:45 +0300

    New Solutions

--- src/U1_StacksAndQueues/L1_Lab/P01_BrowserHistory.java
+++ src/U1_StacksAndQueues/L1_Lab/P01_BrowserHistory.java
@@ -1,7 +1,32 @@
 package U1_StacksAndQueues.L1_Lab;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class P01_BrowserHistory {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> history = new ArrayDeque<>();
+
+        String line = scanner.nextLine();
+
+        while (!"Home".equals(line)) {
+
+            if (!"back".equals(line)) {
+                history.push(line);
+                System.out.println(line);
+
+            } else {
+                if (history.size() <= 1) {
+                    System.out.println("no previous URLs");
+                } else {
+                    history.pop();
+                    System.out.println(history.peek());
+                }
+            }
+
+            line = scanner.nextLine();
+        }
     }
 }
