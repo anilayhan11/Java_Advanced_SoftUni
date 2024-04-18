1b4ae60c0efbe2cd2cf78616a19168b557affb51
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-04-18 13:57:09 +0300

    New Solutions

--- src/U1_StacksAndQueues/L2_Exercise/P09_PoisonousPlants.java
+++ src/U1_StacksAndQueues/L2_Exercise/P09_PoisonousPlants.java
@@ -2,7 +2,7 @@ package U1_StacksAndQueues.L2_Exercise;
 
 import java.util.Scanner;
 
-public class P08 {
+public class P09_PoisonousPlants {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
