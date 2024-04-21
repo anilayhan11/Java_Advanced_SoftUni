53e6e90da603d99839cfd37ee7b405fa24f9d054
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-04-21 23:49:27 +0300

    Update

--- src/U1_StacksAndQueues/L2_Exercise/P09_PoisonousPlants.java
+++ src/U1_StacksAndQueues/L2_Exercise/P09_PoisonousPlants.java
@@ -1,11 +1,57 @@
 package U1_StacksAndQueues.L2_Exercise;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class P09_PoisonousPlants {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
+        int plants = Integer.parseInt(scanner.nextLine());
+        int[] pesticideArr = Arrays
+                .stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
 
+        ArrayDeque<Integer> pesticideStack = new ArrayDeque<>();
+        ArrayDeque<Integer> alivePlantsStack = new ArrayDeque<>();
 
+        for (int entry : pesticideArr) {
+            pesticideStack.push(entry);
+        }
+
+        int daysWithDeath = 0;
+        boolean isThereDeathPlants = true;
+
+        while (isThereDeathPlants) {
+
+            while (pesticideStack.size() != 1) {
+                int plantOneWithPesticide = pesticideStack.pop();
+                int plantTwoWithPesticide = pesticideStack.peek();
+
+                if (plantOneWithPesticide <= plantTwoWithPesticide) {
+                    alivePlantsStack.push(plantOneWithPesticide);
+                }
+            }
+
+            for (Integer entry : alivePlantsStack) {
+                pesticideStack.push(alivePlantsStack.pop());
+            }
+
+            if (pesticideStack.size() != plants) {
+                isThereDeathPlants = true;
+
+            } else {
+                isThereDeathPlants = false;
+
+            }
+            plants = pesticideStack.size();
+
+            if (isThereDeathPlants) {
+                daysWithDeath++;
+            }
+        }
+
+        System.out.println(daysWithDeath);
     }
 }
---
+++ src/U1_StacksAndQueues/L2_Exercise/P09_PoisonousPlants2.java
@@ -0,0 +1,54 @@
+package U1_StacksAndQueues.L2_Exercise;
+
+import java.util.*;
+
+public class P09_PoisonousPlants2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int[] pesticides = Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        ArrayDeque<Integer> plants = new ArrayDeque<>();
+        Arrays.stream(pesticides).forEach(plants::offer);
+
+        int days = 0;
+        while (true) {
+            List<Integer> plantList = new ArrayList<>();
+            int current = plants.poll();
+            plantList.add(current);
+
+            for (int i = 0; i < plants.size(); i++) {
+                if (current >= plants.getFirst()) {
+                    plantList.add(plants.getFirst());
+                }
+
+                current = plants.poll();
+                i--;
+            }
+            days++;
+
+            if (isAscending(plantList)) {
+                break;
+            }
+
+            plantList.forEach(plants::offer);
+        }
+
+        System.out.println(days);
+    }
+
+    public static boolean isAscending(List<Integer> plantsList) {
+        for (int i = 0; i < plantsList.size() - 1; i++) {
+            int first = plantsList.get(i);
+            int second = plantsList.get(i + 1);
+
+            if (first < second) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
