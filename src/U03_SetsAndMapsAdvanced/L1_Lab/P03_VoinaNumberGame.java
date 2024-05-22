89039719a60fae85655dfa6aa9d142433987226d
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-05-22 19:34:11 +0300

    New solutions

---
+++ src/U03_SetsAndMapsAdvanced/L1_Lab/P03_VoinaNumberGame.java
@@ -0,0 +1,54 @@
+package U03_SetsAndMapsAdvanced.L1_Lab;
+
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P03_VoinaNumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        LinkedHashSet<Integer> firstDeck = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        LinkedHashSet<Integer> secondDeck = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .collect(Collectors.toCollection(LinkedHashSet::new));
+
+        int rounds = 50;
+
+        while (rounds-- > 0) {
+            int firstCard = firstDeck.iterator().next();
+            firstDeck.remove(firstCard);
+            int secondCard = secondDeck.iterator().next();
+            secondDeck.remove(secondCard);
+
+            if (firstCard > secondCard) {
+                firstDeck.add(firstCard);
+                firstDeck.add(secondCard);
+            } else if (secondCard > firstCard) {
+                secondDeck.add(firstCard);
+                secondDeck.add(secondCard);
+            }
+
+            if (firstDeck.isEmpty() || secondDeck.isEmpty()) {
+                break;
+            }
+        }
+
+        String output;
+
+        if (firstDeck.size() > secondDeck.size()) {
+            output = "First player win!";
+        } else if (secondDeck.size() > firstDeck.size()) {
+            output = "Second player win!";
+        } else {
+            output = "Draw!";
+        }
+
+        System.out.println(output);
+
+    }
+}
