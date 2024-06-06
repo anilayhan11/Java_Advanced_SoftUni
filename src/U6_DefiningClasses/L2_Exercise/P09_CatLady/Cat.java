de043a6bd4699a4e1dc3d55812bafb807ab39626
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-06-06 19:02:37 +0300

    New solutions

---
+++ src/U6_DefiningClasses/L2_Exercise/P09_CatLady/Cat.java
@@ -0,0 +1,17 @@
+package U6_DefiningClasses.L2_Exercise.P09_CatLady;
+
+public class Cat {
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
