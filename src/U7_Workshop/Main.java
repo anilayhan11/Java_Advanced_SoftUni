ea609c7d183adf9becf2ea50e37dedbee9382759
Anıl Ayhan <anilayhan11@users.noreply.github.com>
2024-06-08 15:51:53 +0300

    New solutions

---
+++ src/U7_Workshop/Main.java
@@ -0,0 +1,15 @@
+package U7_Workshop;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SmartStack stack = new SmartStack();
+
+        stack.push(13);
+        stack.push(42);
+        stack.push(69);
+        stack.push(73);
+
+        stack.forEach(System.out::println);
+    }
+}
